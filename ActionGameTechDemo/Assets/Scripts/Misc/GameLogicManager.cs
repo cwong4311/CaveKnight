fa8d93582c4d7d046e3dda84c5165d57f74ef535
Charlton Wong <cwong4311@gmail.com>
2024-07-06 02:09:03 +1000

    [HUD] Added Victory Screen with scoring. Displays 5 seconds after beating the Dragon boss. - TODO: Add victory fanfare.

--- ActionGameTechDemo/Assets/Scripts/Misc/GameLogicManager.cs
+++ ActionGameTechDemo/Assets/Scripts/Misc/GameLogicManager.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Audio;
@@ -19,6 +20,12 @@ public class GameLogicManager : MonoBehaviour
     public AudioMixerSnapshot GameOverMixerSnapshot;
     public AudioMixerSnapshot PausedMixerSnapshot;
 
+    public GameObject EnemyPool;
+
+    public TextMeshProUGUI FinalScore;
+    public TextMeshProUGUI TimeTaken;
+    public TextMeshProUGUI EnemiesSlain;
+
     public static Action OnPause;
     public static Action OnGameOver;
     public static Action OnLevelComplete;
@@ -27,6 +34,7 @@ public class GameLogicManager : MonoBehaviour
 
     private Coroutine _currentGameEndScreen;
     private float _originalTimeScale;
+    private int _enemiesAtGameStart;
 
     public void OnEnable()
     {
@@ -39,6 +47,8 @@ public class GameLogicManager : MonoBehaviour
 
         if (MenuAudio == null) MenuAudio = GetComponent<AudioSource>();
         DefaultMixerSnapshot.TransitionTo(.01f);
+
+        _enemiesAtGameStart = EnemyPool.GetComponentsInChildren<EnemyHealth>().Length;
 
         OnGameOver += PlayGameOverScreen;
         OnLevelComplete += PlayLevelCompletedScreen;
@@ -80,10 +90,15 @@ public class GameLogicManager : MonoBehaviour
         if (_currentGameEndScreen != null) return;
 
         _currentGameEndScreen = StartCoroutine(
-            PerformAfterDelay(3f,
+            PerformAfterDelay(5,
             () => {
+                OnPause = null;
                 MenuAudio?.PlayOneShot(YouWinSound);
+                SetWinScreenStats();
                 PlayMenuAnimation("YouWin");
+
+                Cursor.visible = true;
+                Cursor.lockState = CursorLockMode.None;
             })
         );
     }
@@ -103,6 +118,51 @@ public class GameLogicManager : MonoBehaviour
     private void ReloadLevel()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    protected void SetWinScreenStats()
+    {
+        float t = Time.timeSinceLevelLoad;
+        float milliseconds = (Mathf.Floor(t * 100) % 100); // calculate the milliseconds for the timer
+        int seconds = (int)(t % 60); // return the remainder of the seconds divide by 60 as an int
+        t /= 60; // divide current time y 60 to get minutes
+        int minutes = (int)(t % 60); //return the remainder of the minutes divide by 60 as an int
+        t /= 60; // divide by 60 to get hours
+        int hours = (int)(t % 24); // return the remainder of the hours divided by 60 as an int
+
+        string timeTaken = "";
+        if (hours > 0)
+        {
+            timeTaken = hours.ToString() + "h " + minutes.ToString() + "m " + seconds.ToString() + "s " + milliseconds.ToString() + "ms ";
+        }
+        else if (minutes > 0)
+        {
+            timeTaken = minutes.ToString() + "m " + seconds.ToString() + "s " + milliseconds.ToString() + "ms ";
+        }
+        else
+        {
+            timeTaken = seconds.ToString() + "s " + milliseconds.ToString() + "ms ";
+        }
+        TimeTaken.text = timeTaken;
+
+        var enemiesAtGameEnd = EnemyPool.GetComponentsInChildren<EnemyHealth>().Length - 1; //Boss won't get destroyed
+        var enemiesKilled = _enemiesAtGameStart - enemiesAtGameEnd;
+        EnemiesSlain.text = enemiesKilled.ToString();
+
+        var baseScore = 100;
+        baseScore -= (enemiesAtGameEnd / 2);
+        baseScore -= ((hours * 60) + (minutes * 1));
+
+        var finalScore = baseScore switch
+        {
+            >90 => "SS",
+            >80 => "S",
+            >70 => "A",
+            >60 => "B",
+            >50 => "C",
+            _ => "D"
+        };
+        FinalScore.text = finalScore;
     }
 
     protected void OnPausePressed()
